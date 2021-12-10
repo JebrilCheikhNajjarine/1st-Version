ebd7fcb6cbbb56d5c09762d04d711c14e1b79156
JebrilCheikhNajjarine <88228767+JebrilCheikhNajjarine@users.noreply.github.com>
2021-12-10 14:46:00 +0200

    Add files via upload
    7th

---
+++ index.js
@@ -0,0 +1,11 @@
+/* incremant numbers*/
+$('.number').each(function () {
+    var $this = $(this);
+    jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+      duration:2000,
+      easing: 'swing',
+      step: function () {
+        $this.text(Math.ceil(this.Counter));
+      }
+    });
+  });
